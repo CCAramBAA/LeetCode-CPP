a3bcb12e6a5fd8497a11bb90693cbabe5f3f9464
CCAramBAA <taopuying2002@gmail.com>
2026-04-12 17:13:58 +0800

    2.回文数

--- 9.回文数.cpp
+++ 9.回文数.cpp
@@ -15,6 +15,12 @@
 #include <climits>
 
 using namespace std;
+
+int main() {
+    Solution sol;
+    return 0;
+}
+
 /*
  * @lc app=leetcode.cn id=9 lang=cpp
  *
@@ -25,7 +31,18 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        if(x<0){
+            return false;
+        }
+        string str=to_string(x);
+        for(int i=0;i<str.size();i++){
+            if(str[i]==str[str.size()-i-1]){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
 };
 // @lc code=end
